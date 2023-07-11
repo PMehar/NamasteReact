ed50c4efd75f703937bae70fd21d7531c07a216a
PMehar <pujamehar05@gmail.com>
2023-07-11 18:47:15 +0530

    Created a functional component of the same with JSX

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const header =(
-  <div className="title">
+// const header =(
+//   <div className="title">
+//     <h1>Namaste React</h1>
+//     <h2>Namaste React</h2>
+//     <h3>Namaste React</h3>
+//   </div>
+// );
+
+//React Component
+//Functional component (New way of writing code)
+const HeaderComponent = ()=>
+  (<div className="title">
     <h1>Namaste React</h1>
     <h2>Namaste React</h2>
     <h3>Namaste React</h3>
-  </div>
-)
+  </div>);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(header); //paasing multiple child using container inside the root (takes in react element and modifies the dom)
+root.render(<HeaderComponent/>); //paasing multiple child using container inside the root (takes in react element and modifies the dom)
