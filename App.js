e3df1627ebe4afb24304250a86edd423e04a4c73
PMehar <pujamehar05@gmail.com>
2023-07-11 16:37:53 +0530

    created same element using JSX

--- App.js
+++ App.js
@@ -1,36 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React.createElement =>Object =>HTML(DOM)
-const heading = React.createElement(
-  "div",
-  {
-    class: "header",
-  },
-  [React.createElement(
-    "h1",
-    {
-      class: "title",
-      key:"h1",
-    },
-    "Namaste React.."
-  ),React.createElement(
-    "h2",
-    {
-      class: "title",
-      key:"h2",
-    },
-    "Namaste React..",
-  ),React.createElement(
-    "h3",
-    {
-      class: "title",
-      key:"h3",
-    },
-    "Namaste React..",
-  )]
-);
-
+const header =(
+  <div className="title">
+    <h1>Namaste React</h1>
+    <h2>Namaste React</h2>
+    <h3>Namaste React</h3>
+  </div>
+)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading); //paasing multiple child using container inside the root (takes in react element and modifies the dom)
+root.render(header); //paasing multiple child using container inside the root (takes in react element and modifies the dom)
