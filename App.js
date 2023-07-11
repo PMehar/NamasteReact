aa878413b4e88103087e9dab608e7b4a9e6de7bb
PMehar <pujamehar05@gmail.com>
2023-07-11 18:58:21 +0530

    Passed attributes into the tag in JSX

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const header =(
-//   <div className="title">
-//     <h1>Namaste React</h1>
-//     <h2>Namaste React</h2>
-//     <h3>Namaste React</h3>
-//   </div>
-// );
+const header =(
+  <div className="title">
+    <h1>Hii there</h1>
+    <h2>This is react element</h2>
+    <h3>And it is using JSX</h3>
+  </div>
+);
 
 //React Component
 //Functional component (New way of writing code)
 const HeaderComponent = ()=>
-  (<div className="title">
-    <h1>Namaste React</h1>
-    <h2>Namaste React</h2>
-    <h3>Namaste React</h3>
+  (<div >
+    {header}
+    <h1>Namaste React functional component</h1>
+    <h4>using JSX</h4>
   </div>);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
