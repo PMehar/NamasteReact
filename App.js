9e4193dcead96f19c9cc472a43a31216dc956813
PMehar <pujamehar05@gmail.com>
2023-07-12 10:55:59 +0530

    inline styling in react

--- App.js
+++ App.js
@@ -1,23 +1,93 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const TitleComponent = ()=>(
-  <div className="title">
-    <h1>Hii there</h1>
-    <h2>This is react element</h2>
-    <h3>And it is using JSX</h3>
-  </div>
+/*
+      Header
+        -Logo
+        -Nav Items(Right side)
+        -cart
+      Body
+        -search bar
+        -RestaurantList
+          -RestaurantCard
+            -Image
+            -Name
+            -Rating
+            -cusines
+      Footer
+        -Links
+        -Copyrights
+    */
+   //React.Fragment
+   //jsx has only one parent
+
+const Title = () => (
+  <a href="/">
+    <img
+      className="logo"
+      alt="logo"
+      src="https://cdn.vectorstock.com/i/preview-1x/43/01/restaurant-map-pointer-icon-food-point-pin-symbol-vector-47134301.webp"
+    />
+  </a>
 );
 
-//React Component
-//Functional component (New way of writing code)
-const HeaderComponent = ()=>
-  (<div >
-    <TitleComponent/>
-    <h1>Namaste React functional component</h1>
-    <h4>using JSX</h4>
-  </div>);
+//composing component
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const Body = () => {
+  return <h1>Body</h1>;
+};
+
+const Footer = () => {
+  return <h1>Footer</h1>;
+};
+const styleObj ={ //as an obj
+  backgroundColor : "red",
+}
+
+// const AppLayout = () => {
+//   return (
+//     <React.Fragment>
+//       <Header />
+//       <Body />
+//       <Footer />
+//     </React.Fragment>
+//   );
+// };
+
+//React.Fragment
+//jsx has only one parent
+/*1 way of inline styling in react*/
+
+const Jsx =()=>(
+   <div style ={styleObj}>  
+    <h1>JSX Here</h1>
+  </div>
+)
+/**2nd way of inline styling in react*/
+const jsx2 =(
+  <div style ={{
+    backgroundColor : "green" ,
+  }}>
+    {Jsx()}
+    <h1>JSX Here</h1>
+    <h2>h2</h2>
+  </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeaderComponent/>); //paasing multiple child using container inside the root (takes in react element and modifies the dom)
+root.render(jsx2); //paasing multiple child using container inside the root (takes in react element and modifies the dom)
